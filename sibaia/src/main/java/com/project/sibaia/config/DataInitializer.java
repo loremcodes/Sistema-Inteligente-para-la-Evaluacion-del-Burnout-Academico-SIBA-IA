3a0c749b1b68f5d3864f394c75fb0674b2f9d3e6
Lorena Soto Zenteno <lsotozenteno@gmail.com>
2026-08-21 19:40:00 -0500

    actualización de configuración

--- sibaia/src/main/java/com/project/sibaia/config/DataInitializer.java
+++ sibaia/src/main/java/com/project/sibaia/config/DataInitializer.java
@@ -1,17 +1,22 @@
 package com.project.sibaia.config;
 
+import com.project.sibaia.entity.EscuelaProfesional;
 import com.project.sibaia.repository.AdministradorRepository;
 import com.project.sibaia.entity.Administrador;
+import com.project.sibaia.repository.EscuelaProfesionalRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
 
 @Configuration
 public class DataInitializer {
     @Bean
     CommandLineRunner initDatabase(
             AdministradorRepository adminRepo,
+            EscuelaProfesionalRepository escuelasRepo,
             PasswordEncoder passwordEncoder
     ) {
         return args -> {
@@ -23,6 +28,20 @@ public class DataInitializer {
                 admin.setPassword(passwordEncoder.encode("admin123"));
 
                 adminRepo.save(admin);
+            }
+            if (escuelasRepo.count() == 0) {
+                List<String> nombresEscuelas = List.of(
+                        "Contabilidad",
+                        "Administración",
+                        "Derecho",
+                        "Ingeniería de Sistemas"
+                );
+
+                for (String nombre : nombresEscuelas) {
+                    EscuelaProfesional ep = new EscuelaProfesional();
+                    ep.setNombre(nombre);
+                    escuelasRepo.save(ep);
+                }
             }
         };
     }
